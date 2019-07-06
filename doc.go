b4c9b0c98b253b6c3ab4c3c15a12abc55b02d7da
Srinidhi Kaushik <shrinidhi.kaushik@gmail.com>
2019-07-06 16:31:17 +0530

    Nitpicks.

--- doc.go
+++ doc.go
@@ -2,19 +2,19 @@
 Command network-check sends pod connection failure events to DataDog.
 
 Usage:
-	pod-network-check -name {C} -namespace {N} -ports {X,Y,Z...}
-	                  -dogstatsd {D} -interval {I} -timeout {T}
-	                  -protocol {P} -debug
+    pod-network-check -name {C} -namespace {N} -ports {X,Y,Z...}
+                      -dogstatsd {D} -interval {I} -timeout {T}
+                      -protocol {P} -debug
 
 Arguments:
-	-name        A name for the check; used the aggregation key.
-	-namespace   The `kubernetes' namespace to check for pods.
-	-ports       Comma separated list of ports to check.
-	-dogstatsd   Address to the `dogstatsd' server.
-	-interval    Check run interval, in seconds.
-	-timeout     Dial timeout for pods, in seconds.
-	-protocol    Protocol to use for the check.
-	-debug       Print debug output.
+    -name        A name for the check; used the aggregation key.
+    -namespace   The `kubernetes' namespace to check for pods.
+    -ports       Comma separated list of ports to check.
+    -dogstatsd   Address to the `dogstatsd' server.
+    -interval    Check run interval, in seconds.
+    -timeout     Dial timeout for pods, in seconds.
+    -protocol    Protocol to use for the check.
+    -debug       Print debug output.
 
 Defaults:
     -name        pod-network-check
